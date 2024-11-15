ae97a2ca512a05c29870cb0d958bda87460d69a5
iam-sphinx <kandarp1803@gmail.com>
2024-11-15 13:47:14 +0530

    minor fix

--- src/lib/customFetch.ts
+++ src/lib/customFetch.ts
@@ -1,6 +1,6 @@
 type FetchOptions = Omit<RequestInit, "method" | "body"> & {
   method?: "GET" | "POST" | "PUT" | "DELETE";
-  body?: Record<string, any> | string;
+  body?: Record<string, any> | string | FormData;
 };
 
 const customFetch = async <T>(
@@ -9,22 +9,28 @@ const customFetch = async <T>(
 ): Promise<T> => {
   try {
     const baseUrl = process.env.NEXT_PUBLIC_SERVER_URL || "";
-
     const url = `${baseUrl}${endpoint}`;
 
     let finalBody = undefined;
+
+    // Handle body depending on its type
     if (body instanceof FormData) {
-      finalBody = body; // FormData should not be stringified
+      finalBody = body; 
     } else if (body) {
-      finalBody = JSON.stringify(body); // Default to stringifying JSON bodies
+      finalBody = JSON.stringify(body); 
     }
+
+    // Conditional Content-Type header based on body type
+    const finalHeaders = {
+      ...headers,
+      ...(body instanceof FormData
+        ? {}
+        : { "Content-Type": "application/json" }), // Only set Content-Type for non-FormData
+    };
 
     const response = await fetch(url, {
       method,
-      headers: {
-        // "Content-Type": "application/json",
-        ...headers,
-      },
+      headers: finalHeaders,
       body: finalBody,
       ...options,
       credentials: "include",
